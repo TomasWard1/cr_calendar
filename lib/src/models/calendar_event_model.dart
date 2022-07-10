fb83225725d1461c5853db33ed1dfc20588e8b41
Tomas Ward <35110844+TomasWard1@users.noreply.github.com>
2022-07-09 23:57:24 -0300

    Update calendar_event_model.dart

--- lib/src/models/calendar_event_model.dart
+++ lib/src/models/calendar_event_model.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class CalendarEventModel {
   CalendarEventModel({
+    required this.id,
     required this.name,
     required this.begin,
     required this.end,
@@ -9,7 +10,15 @@ class CalendarEventModel {
   });
 
   String name;
+  String id;
   DateTime begin;
   DateTime end;
   Color eventColor;
+
+
+  @override
+  bool operator ==(other) {
+    return (other is CalendarEventModel) && other.id == id;
+  }
+
 }
